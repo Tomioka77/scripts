3b15c01eb76580fd4507cfae382739478561c0fa
Mezaque <57005067+Myydata@users.noreply.github.com>
2021-10-19 21:44:49 -0300

    android12

---
+++ vendorsetup.sh
@@ -0,0 +1,8 @@
+# Kernel
+git clone --depth=1 https://github.com/silont-project/kernel_xiaomi_surya -b staging-backup kernel/xiaomi/surya
+
+# Vendor
+git clone --depth=1 https://github.com/subhajeetmuhuri/proprietary_vendor_xiaomi_surya -b lineage-19.0 vendor/xiaomi/surya
+
+# Vendor
+git clone --depth=1 https://github.com/silont-project/silont-clang -b master prebuilts/clang/host/linux-x86/silont
